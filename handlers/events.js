a236568785ff937f6d9b464f4dbdeb7e8a52f1c6
xkmxz <aw53978296@163.com>
2026-03-24 18:15:36 +0800

    终于把子数组的值读出来了

--- handlers/events.js
+++ handlers/events.js
@@ -1,6 +1,7 @@
 /// <reference path="../easybot-sdk/easybot.d.ts" />
-const { formatTime, getConfigure } = require('../utils/helper.js');
+const { formatTime, parseMcsmConfig } = require('../utils/helper.js');
 const { getAllConfig } = require('../utils/config.js');
+
 
 function registerEvents() {
   bus.on('enable', () => {
@@ -16,13 +17,16 @@ function registerEvents() {
 function botEvent() {
   bus.on("group_message_event", (event) => {
     const data = config.getConfigValue("mcsmconfig", "mcsm_config")
+    const { masterNodes, childNodes, relations } = parseMcsmConfig();
     if (!data) {
       logger.info("[MCSM配置检测] 错误：未获取到 mcsm_config 数据");
     } else {
       // 2. 输出数据长度（数组用 length，对象用键数量）
       const length = Array.isArray(data) ? data.length : Object.keys(data).length;
       logger.info("[MCSM配置检测] 数据长度: {0}", length);
-      const serverName = 1111
+      for (const instance of childNodes) {
+      const serverName = instance.serverName;
+      logger.info("[MCSM配置检测] 子节点 serverName: {0}", serverName);
       if (event.RawMessage === "开启 " + serverName && event.PeerId === "526191124") {
         event.Context.Reply(new MessageChain().Text("pong")
           .Text("当前配置：")
@@ -36,6 +40,7 @@ function botEvent() {
             event.Context.Reply(new MessageChain().Text("getConfigure()"));
           }
       }
+    }
     
   });
 }
